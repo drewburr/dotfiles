5c0f1efff77a085a2bc60ea65a86e46c63088b13
drewburr <drewburr9@gmail.com>
2021-01-05 13:41:49 -0600

    Add ls aliases

--- ls_alias.sh
+++ ls_alias.sh
@@ -3,7 +3,9 @@
 alias ls='ls -G'  # Colorised output
 alias ll='ls -l'  # Long output
 alias la='ls -la' # Long with all
-alias lla='ls -la' # Long with all
-alias lld='ls -ld' # Long and direct
+alias lla='ls -la'
+alias ld='ls -ld' # Long and direct
+alias lld='ls -ld'
 alias lt='ls -lt' # Long and sorted by time
 alias ltr='ls -ltr' # Long and sorted by time (reversed)
+alias latr='ls -latr' # Long, all, and sorted by time (reversed)
