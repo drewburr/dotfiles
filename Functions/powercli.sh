e90e3c271df6aae497791e213a91d3edcf16a96e
drewburr <drewburr9@gmail.com>
2021-01-06 12:32:33 -0600

    Convert variables to be local

--- Functions/powercli.sh
+++ Functions/powercli.sh
@@ -2,11 +2,11 @@
 # Drops you into a docker container with powercli installed.
 # This is primairily used to interact with vmware
 
-local_volume='/Users/browndr/Projects/docker/vmware-powerclicore'
-container_volume='/usr/scripts'
-
 # powercli container
 function vmware() {
+    local_volume='/Users/browndr/Projects/docker/vmware-powerclicore'
+    container_volume='/usr/scripts'
+
     echo Local files: $local_volume
     echo Container files: $container_volume
     docker run -it --rm --volume $local_volume:$container_volume vmware/powerclicore:latest
