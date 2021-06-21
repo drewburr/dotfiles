1a71b62e070eadd51c9ce4be7349728e6853fb3a
drewburr <drewburr9@gmail.com>
2021-06-21 16:15:20 -0500

    Add k8s autocomplete

---
+++ k8s.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://kubernetes.io/docs/reference/kubectl/cheatsheet/#kubectl-autocomplete
+
+# bash-completion package should be installed for this to work
+source <(kubectl completion bash)
