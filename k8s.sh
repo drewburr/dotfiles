d45d4c821e80f044ab50dc127ce113f06e63517a
drewburr <drewburr9@gmail.com>
2021-08-24 11:35:14 -0500

    Update comment to be more specific

--- k8s.sh
+++ k8s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-bash-mac/
-# This requires Bash 4.1+, which was installed via Homebrew
+# This requires Bash 4.1+, which was installed via Homebrew (MacOS)
 # chsh -s /usr/local/Cellar/bash/5.1.8/bin/bash
 export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
