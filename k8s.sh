d492b683fead3b507b7cf93f122b187e24ed0636
drewburr <drewburr9@gmail.com>
2021-08-24 11:33:32 -0500

    Add kexec, kubectl autocompletion, and aliases

--- k8s.sh
+++ k8s.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# https://kubernetes.io/docs/reference/kubectl/cheatsheet/#kubectl-autocomplete
 
-# bash-completion package should be installed for this to work
+# https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-bash-mac/
+# This requires Bash 4.1+, which was installed via Homebrew
+# chsh -s /usr/local/Cellar/bash/5.1.8/bin/bash
+export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 source <(kubectl completion bash)
+
+# Aliases
+alias k='kubectl'
