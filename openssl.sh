d7a53d643a52bfea48466f15d737412e5d059f36
drewburr <drewburr9@gmail.com>
2021-08-30 13:17:46 -0500

    Add openssl configs

---
+++ openssl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# From homebrew:
+#
+# A CA file has been bootstrapped using certificates from the system
+# keychain. To add additional certificates, place .pem files in
+#   /usr/local/etc/openssl@1.1/certs
+#
+# and run
+#   /usr/local/opt/openssl@1.1/bin/c_rehash
+#
+# openssl@1.1 is keg-only, which means it was not symlinked into /usr/local,
+# because macOS provides LibreSSL.
+#
+# If you need to have openssl@1.1 first in your PATH, run:
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+#
+# For compilers to find openssl@1.1 you may need to set:
+export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+#
+# For pkg-config to find openssl@1.1 you may need to set:
+export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
