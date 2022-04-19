bd76d97d5d84aa0d68ee48965a08f2010801bf93
drewburr <drewburr9@gmail.com>
2022-04-19 13:27:49 -0500

    Only set completion when unset

--- k8s.sh
+++ k8s.sh
@@ -3,7 +3,10 @@
 # https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-bash-mac/
 # This requires Bash 4.1+, which was installed via Homebrew (MacOS)
 # chsh -s /usr/local/Cellar/bash/5.1.8/bin/bash
-export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+
+if [[ -z $BASH_COMPLETION_COMPAT_DIR ]]; then
+    export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+fi
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 source <(kubectl completion bash)
 
