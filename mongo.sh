ecd2fe9602466f781a9c484d8f5fed6686ef8fb6
drewburr <drewburr9@gmail.com>
2022-06-01 10:56:57 -0400

    Remove mongo docker alias

--- mongo.sh
+++ mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Drops you into a docker container with mongo-cli installed.
 
-alias mongo="docker run -it --rm mongo:latest bash"
+# alias mongo="docker run -it --rm mongo:latest bash"
