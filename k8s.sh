bef4dde862d24b71302d558144c9eee913fbafca
drewburr <drewburr9@gmail.com>
2023-01-04 09:22:20 -0600

    Update example command with ln

--- k8s.sh
+++ k8s.sh
@@ -3,6 +3,8 @@
 # https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-bash-mac/
 # This requires Bash 4.1+, which was installed via Homebrew (MacOS)
 # chsh -s /usr/local/Cellar/bash/5.1.8/bin/bash
+# OR
+# chsh -s /usr/local/bin/bash
 
 if [[ -z $BASH_COMPLETION_COMPAT_DIR ]]; then
     export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
