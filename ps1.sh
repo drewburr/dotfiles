665cef7a86ac60743859ea18b697d3e9e71929cf
drewburr <drewburr9@gmail.com>
2023-08-01 10:45:44 -0500

    Implement kube-ps1

--- ps1.sh
+++ ps1.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 # bash
-export PS1="\[\033[01;32m\]\u@localhost\[\033[01;34m\] \W\[\033[00m\] "
+DEFAULT_PS1="\[\033[01;32m\]\u@localhost\[\033[01;34m\] \W\[\033[00m\] "
+export PS1=$DEFAULT_PS1
 
 # zsh
 # export PS1="%n@localhost %1~ %# "
+
+function get_namespace() {
+  echo $(kubens -c)
+}
+
+function get_cluster() {
+  echo $(kubectl config current-context)
+}
+
+KUBEPS1_PATH="/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+if [[ -f $KUBEPS1_PATH ]]; then
+    export PS1_SUFFIX="\[\033[01;34m\] \W\[\033[00m\] "
+    export KUBE_PS1_CLUSTER_FUNCTION=get_cluster
+    export KUBE_PS1_NAMESPACE_FUNCTION=get_namespace
+    export KUBE_PS1_SUFFIX=")"
+    export KUBE_PS1_SYMBOL_ENABLE="false"
+    source $KUBEPS1_PATH
+    export KUBE_PS1='$(kube_ps1)'$PS1_SUFFIX
+    export PS1=$KUBE_PS1
+
+    alias kubeoff="kubeoff && export PS1=\"$DEFAULT_PS1\""
+    alias kubeon="kubeon && export PS1=\"$KUBE_PS1\""
+    kubeon
+fi
