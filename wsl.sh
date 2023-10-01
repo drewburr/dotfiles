9253cd0a87b2984098567c273e28136a197c3ef8
drewburr <drew@drewburr.com>
2023-10-01 16:42:42 -0400

    Check if /proc/version exists before using

--- wsl.sh
+++ wsl.sh
@@ -2,7 +2,7 @@
 
 # Needed when Windows hosts paths are being ignored
 # Default (enabled) causes bash completion performance concerns
-if [[ $(grep -i Microsoft /proc/version) ]]; then
+if [[ -f /proc/version && $(grep -i Microsoft /proc/version) ]]; then
     # echo "Bash is running on WSL"
     alias code='/mnt/c/Users/drewb/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
     alias explorer.exe='/mnt/c/Windows/explorer.exe'
