c3d832a789c6d7062d8cc1c8074a57a546fa75fb
drewburr <drew@drewburr.com>
2023-10-02 22:55:30 -0400

    Add python versions

--- PATH.sh
+++ PATH.sh
@@ -3,8 +3,9 @@
 # Puppet SDK
 PATH=$PATH:/opt/puppetlabs/pdk/bin/
 
-# Python3.7
-PATH=$PATH:$HOME/Library/Python/3.7/bin
+# Python
+PATH=$PATH:$HOME/Library/Python/3.7/bin:$HOME/Library/Python/3.8/bin:$HOME/Library/Python/3.9/bin
+PATH=$PATH:$HOME/Library/Python/3.10/bin:$HOME/Library/Python/3.11/bin
 
 # Oracle Instant Client
 PATH=$PATH:$HOME/Library/instantclient_19_3
