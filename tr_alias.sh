abe1144f146c835020f957d67b2b44a9c512b023
browndr <drew.brown@wwt.com>
2025-01-08 12:44:04 -0600

    Fix alias

--- tr_alias.sh
+++ tr_alias.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-alias upper='tr [:lower:] [:upper:]'  # Force upper case
-alias lower='tr [:upper:] [:lower:]'  # Force lower case
+alias upper="tr '[:lower:]' '[:upper:]'"  # Force upper case
+alias lower="tr '[:upper:]' '[:lower:]'"  # Force lower case
