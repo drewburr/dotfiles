e1c3a7e9a6c373d4b72b7c26a839fef3bf53b8d4
drewburr <drew@drewburr.com>
2025-04-28 08:36:17 -0500

    Fix script sourcing

--- source_scripts.sh
+++ source_scripts.sh
@@ -2,15 +2,15 @@
 # This sets an alias for every script in $DOTFILES_PATH/Scripts/* based on file name
 # The file type suffix is omitted from the alias
 
-for item in $(ls $DOTFILES_PATH/Scripts/* | xargs -n1 basename); do
-	# Ensure item is a file
-	if [ -f $item ]; then
-		# Ensure file is executable
-		chmod +x $item
-		# Strip file type suffix
-		cmd=${var%\.*}
-		# Set alias
-		echo $cmd
-		alias $cmd="$item"
-	fi
+for item in $(ls $DOTFILES_PATH/Scripts/*); do
+    # Ensure item is a file
+    if [ -f $item ]; then
+        # Ensure file is executable
+        chmod +x $item
+        # Strip file extension
+        filename=$(basename $item)
+        cmd=${filename%\.*}
+        # Set alias
+        alias $cmd="$item"
+    fi
 done
