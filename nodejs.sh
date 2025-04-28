6b6585a2ec7937720eb2c70c1aed2e64e64509de
drewburr <drew@drewburr.com>
2025-04-28 08:37:23 -0500

    Only load NVM when required

--- nodejs.sh
+++ nodejs.sh
@@ -3,14 +3,28 @@
 export PATH="/usr/local/opt/node@14/bin:$PATH"
 HOMEBREW_PATH="$(command -v brew)"
 
-# nvm setup, reccommended by homebrew
-if [[ -d $(echo $HOMEBREW_PATH | sed 's/\/bin\/brew//')/opt/nvm/ ]]; then
-    # Activating NVM is expensive. Use wrapper function to only load when needed
-    function nvm_enable() {
-        echo "Enabling NVM..."
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"                                       # This loads nvm
-        [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
-        echo "NVM is ready."
-    }
-fi
+lazynvm() {
+    unset -f nvm node npm
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+}
+
+nvm() {
+    lazynvm
+    nvm $@
+}
+
+node() {
+    lazynvm
+    node $@
+}
+
+npm() {
+    lazynvm
+    npm $@
+}
+
+npx() {
+    lazynvm
+    npx $@
+}
